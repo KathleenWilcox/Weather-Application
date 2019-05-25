6e00f17f0606c4e6db31189ab089011dac08b1cc
Katie Wilcox <katiewilcox@katies-macbook-pro-2.local>
2019-05-25 10:55:16 -0400

    Initial Commit

---
+++ WeatherApplication/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherApplication
+//
+//  Created by Katie Wilcox on 5/25/19.
+//  Copyright © 2019 Katie Wilcox. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
